e42714bbe48b8bfee9148de0d13e947146444251
Manjiri Vadujkar <manjiri.vadujkar@gmail.com>
2023-01-03 10:18:40 +0530

    Initial UI Skeleton

--- src/App.js
+++ src/App.js
@@ -1,24 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./images/logo-new.png";
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [fieldVisiblity, setFieldVisiblity] = useState("userInputs");
+  const [resultVisiblity, setResultVisiblity] = useState("resultBox");
+  const [placeholder, setPlaceholder] = useState("");
+
+  function displayUserFields(e) {
+    console.log(e.target.text);
+    //make userFields visible
+    setFieldVisiblity("userInputs-visible");
+    //Change userField input placeholder
+    setPlaceholder(e.target.innerText);
+  }
+
+  function displayResult(e) {
+    setResultVisiblity("resultBox-visible");
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <main>
+      <header className="logo">
+        <img src={logo}></img>
       </header>
-    </div>
+      <div className="userFields container">
+        <div className="userOptions">
+          <button
+            className="button optionsBtn"
+            value="lyricSearch"
+            onClick={(e) => {
+              displayUserFields(e);
+            }}
+          >
+            Search By Lyrics
+          </button>
+          <button
+            className="button optionsBtn"
+            value="songSearch"
+            onClick={(e) => {
+              displayUserFields(e);
+            }}
+          >
+            Search By Song Name
+          </button>
+          <button
+            className="button optionsBtn"
+            value="artistSearch"
+            onClick={(e) => {
+              displayUserFields(e);
+            }}
+          >
+            Search By Artist Name
+          </button>
+        </div>
+        <div className={fieldVisiblity}>
+          <input type="text" placeholder={placeholder}></input>
+          <button
+            className="button"
+            value="submit"
+            name="Search"
+            onClick={(e) => {
+              displayResult(e);
+            }}
+          >
+            Search
+          </button>
+        </div>
+      </div>
+      <div id={resultVisiblity} className="container">
+        <p>dewiobfiwenfwpofncweopfnewofnwe</p>
+      </div>
+    </main>
   );
 }
 
